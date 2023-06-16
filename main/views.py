2709a85f00dd9b3cfa4e32b978e19fa4441315a9
Mohsen <mohsenrazavi.self@gmail.com>
2023-06-16 16:48:42 +0330

    complete add word

--- main/views.py
+++ main/views.py
@@ -8,7 +8,7 @@ def search_word_view(request):
     if request.method == 'POST':
         word = request.POST['word'].strip()
         result = trie_dict.search_word(word)
-        return render(request, 'main/home.html', context={'word': word, 'result': result, 'show_result': True})
+        return render(request, 'main/search_result.html', context={'word': word, 'result': result})
     else:
         return redirect(reverse('home'))
 
@@ -17,13 +17,14 @@ def add_word_view(request):
     context = {}
     if request.method == 'POST':
         word = request.POST['word'].strip()
+        context['word'] = word
         result = trie_dict.add_new_word(word)
         if result:
-            context['status'] = 'successful'
+            context['result'] = True
         else:
-            context['status'] = 'already exists'
+            context['result'] = False
 
-        return render(request, 'main/home.html', context=context)
+        return render(request, 'main/add_result.html', context=context)
     else:
         return redirect(reverse('home'))
 
