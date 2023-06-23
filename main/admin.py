eae044b1c7a5253671291e0db028d9715b98e44a
Mohsen <mohsenrazavi.self@gmail.com>
2023-06-23 21:16:59 +0330

    create word model

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Word
+
+
+@admin.register(Word)
+class WordAdmin(admin.ModelAdmin):
+    fields = [
+        'word',
+        'frequency',
+    ]
+
--- main/models.py
+++ main/models.py
@@ -1,2 +1,7 @@
 from django.db import models
 
+
+class Word(models.Model):
+    word = models.CharField(max_length=127, verbose_name='word')
+    frequency = models.PositiveIntegerField(default=0)
+
